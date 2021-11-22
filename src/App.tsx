a9915c95c992c5acbf2dc51566077b9d08970457
gabriel silva <gabrielp.siilva@gmail.com>
2021-11-22 10:52:51 -0300

    adding New Transaction Modal component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,8 @@
 import { useState } from 'react';
-import Modal from 'react-modal';
 import { Dashboard } from './components/Dashboard';
 import { Header } from './components/Header';
+import { NewTransactionModal } from './components/NewTransactionModal';
 import { GlobalStyle } from './styles/global';
-
-Modal.setAppElement('#root');
 
 export function App() {
   const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] = useState(false);
@@ -16,18 +14,18 @@ export function App() {
   function handleCloseNewTransactionModal() {
     setIsNewTransactionModalOpen(false)
   }
-  
+
   return (
     <>
       <GlobalStyle />
-      <Header onOpenNewTransactionModal={handleOpenNewTransactionModal}/>
+      <Header
+        onOpenNewTransactionModal={handleOpenNewTransactionModal}
+      />
 
-      <Modal
+      <NewTransactionModal
         isOpen={isNewTransactionModalOpen}
         onRequestClose={handleCloseNewTransactionModal}
-      >
-        <h2>Cadastrar transação</h2>
-      </Modal>
+      />
 
       <Dashboard />
     </>
