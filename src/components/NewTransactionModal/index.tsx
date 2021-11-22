6c62d8465fbc61ed2efd06f3163b514320758771
gabriel silva <gabrielp.siilva@gmail.com>
2021-11-22 16:56:46 -0300

    config miragejs with seeds

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,8 +1,9 @@
-import { useState } from 'react';
+import { FormEvent, useState } from 'react';
 import Modal from 'react-modal';
 import closeImg from '../../assets/close.svg';
 import incomeImg from '../../assets/income.svg';
 import outcomeImg from '../../assets/outcome.svg';
+import { api } from '../../services/api';
 import { Container, RadioBox, TransactionTypeContainer } from './styles';
 
 interface NewTransactionModalProps {
@@ -13,8 +14,24 @@ interface NewTransactionModalProps {
 Modal.setAppElement('#root');
 
 export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionModalProps) {
+  const [title, setTitle] = useState('');
+  const [value, setValue] = useState(0);
+  const [category, setCategory] = useState('');
   const [type, setType] = useState('deposit');
-  
+
+  function handleCreateNewTransaction(event: FormEvent): void {
+    event.preventDefault();
+
+    const data = {
+      title,
+      value,
+      category,
+      type
+    };
+
+    api.post('/transactions', data);
+  }
+
   return (
     <Modal
       isOpen={isOpen}
@@ -30,12 +47,19 @@ export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionMo
         <img src={closeImg} alt="Fechar modal" />
       </button>
 
-      <Container>
+      <Container onSubmit={handleCreateNewTransaction}>
         <h2>Cadastrar transação</h2>
 
-        <input placeholder="Título" />
+        <input
+          placeholder="Título"
+          value={title}
+          onChange={event => setTitle(event.target.value)} />
 
-        <input type="number" placeholder="Valor" />
+        <input
+          type="number"
+          placeholder="Valor"
+          value={value}
+          onChange={event => setValue(Number(event.target.value))} />
 
         <TransactionTypeContainer>
           <RadioBox
@@ -43,7 +67,7 @@ export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionMo
             onClick={() => { setType('deposit') }}
             isActive={type === 'deposit'}
             activeColor="green"
-            >
+          >
             <img src={incomeImg} alt="Entrada" />
             <span>Entrada</span>
           </RadioBox>
@@ -59,7 +83,10 @@ export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionMo
           </RadioBox>
         </TransactionTypeContainer>
 
-        <input placeholder="Categoria" />
+        <input
+          placeholder="Categoria"
+          value={category}
+          onChange={event => setCategory(event.target.value)} />
 
         <button type="submit">
           Cadastrar
--- src/mirage-config.ts
+++ src/mirage-config.ts
@@ -1,21 +1,36 @@
-import { createServer } from "miragejs";
+import { createServer, Model } from "miragejs";
 
 export default function mirageConfig() {
   return createServer({
+    models: {
+      transaction: Model,
+    },
+
+    seeds(server) {
+      server.db.loadData({
+        transactions: [
+          {
+            id: 1,
+            title: 'teste',
+            type: 'deposit',
+            category: 'dev',
+            amount: 12,
+            createdAt: new Date('2021-03-20 13:20:00')
+          }
+        ],
+      })
+    },
+
     routes() {
       this.namespace = 'api';
 
       this.get('/transactions', () => {
-        return [
-          {
-            id: 1,
-            title: 'Transaction 1',
-            amount: 400,
-            type: 'deposit',
-            category: 'food',
-            createdAd: new Date()
-          }
-        ]
+        return this.schema.all('transaction')
+      })
+
+      this.post('/transactions', (schema, request) => {
+        const data = JSON.parse(request.requestBody);
+        return schema.create('transaction', data);
       })
     }
   });
