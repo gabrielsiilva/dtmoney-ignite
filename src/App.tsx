94c1819aa4df0342fc784c828213eb2cd46362f1
gabriel silva <gabrielp.siilva@gmail.com>
2021-11-23 10:41:22 -0300

    adding context api

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { Dashboard } from './components/Dashboard';
 import { Header } from './components/Header';
 import { NewTransactionModal } from './components/NewTransactionModal';
 import { GlobalStyle } from './styles/global';
+import { TransactionContext } from './TransactionContext';
 
 export function App() {
   const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] = useState(false);
@@ -16,7 +17,7 @@ export function App() {
   }
 
   return (
-    <>
+    <TransactionContext.Provider value={[]}>
       <GlobalStyle />
       <Header
         onOpenNewTransactionModal={handleOpenNewTransactionModal}
@@ -28,6 +29,6 @@ export function App() {
       />
 
       <Dashboard />
-    </>
+    </TransactionContext.Provider>
   );
 }
--- src/components/TransactionTable/index.tsx
+++ src/components/TransactionTable/index.tsx
@@ -1,5 +1,6 @@
-import { useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import { api } from "../../services/api";
+import { TransactionContext } from "../../TransactionContext";
 import { Container } from "./styles";
 
 interface Transaction {
@@ -12,6 +13,7 @@ interface Transaction {
 }
 
 export function TransactionTable() {
+  const data = useContext(TransactionContext);
   const [transactions, setTransactions] = useState<Transaction[]>([]);
 
   useEffect(() => {
@@ -48,7 +50,7 @@ export function TransactionTable() {
               <td>
                 {new Intl.DateTimeFormat('pt-BR')
                   .format(new Date(transaction.createdAt)
-                )}
+                  )}
               </td>
             </tr>
           ))}
